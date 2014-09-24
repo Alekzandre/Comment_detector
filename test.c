99379afc89ce0b9e53fe8ac1195b72986f79cfa8
azrael <azrael@staff.42.fr>
2014-09-24 10:47:14 +0200

    version revised

---
+++ test.c
@@ -0,0 +1,12 @@
+/* ***************************************** HEADER ********************** */
+// Header aussi "yohou!
+
+// /* Header avec un "petit piege de dans
+
+int main() // Piege aussi "ici!
+{
+  printf("\\"// je suis dans le code");
+  return 0;
+}
+
+/* Code // finale */
